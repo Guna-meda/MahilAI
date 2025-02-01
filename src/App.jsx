2b9f5a31ef1ce304ee9887fb97d4ab4757493ca1
Guna-meda <meda.gunavathi25@gmail.com>
2025-02-01 18:43:23 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -4,37 +4,8 @@ import Footer from "./components/footer";
 import About from "./components/About";
 import { Route, Router, Routes, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Contact from "./components/contact";
+import Contact from "./components/Contactpage";
 import Event from "./components/event";
-//
-//export const App = () => {
-//  const location = useLocation();
-//
-//  const navbarStyle =
-//    location.pathname === "/"
-//      ? "bg-gradient-to-b from-[#130726] to-[#1a0933] text-white"
-//      : "bg-transparent text-black";
-//
-//  return (
-//    <>
-//      <div className="absolute inset-0 bg-gradient-to-b from-black via-transparent to-transparent pointer-events-none z-0"></div>
-//      <Navbar/>
-//
-//      <div className="relative z-20">
-//        <Routes>
-//          <Route path="/" element={<Homepage />} />
-//          <Route path="/about" element={<About />} />
-//          <Route path="/event" element={<Event />} />
-//          <Route path="/contact" element={<Contact/>} />
-//        </Routes>
-//      </div>
-//      <Footer />
-//    </>
-//  );
-//};
-//
-//export default App;
-
 
 const App = () => {
   const location = useLocation();
